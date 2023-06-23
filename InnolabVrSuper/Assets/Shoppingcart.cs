fb9eb8e83e0b3fcc42c49c3ee6a39331b9be99d4
oliver13600 <82397911+oliver13600@users.noreply.github.com>
2023-06-23 18:01:15 +0200

    warenkorb interface

--- InnolabVrSuper/Assets/Shoppingcart.cs
+++ InnolabVrSuper/Assets/Shoppingcart.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.XR.Interaction.Toolkit;
+
 public class Shoppingcart : MonoBehaviour
 {
     public TMP_Text itemCountText; // Reference to the TMP_Text component to display the item count
     private int itemCount = 0; // Current item count
     public GameObject ui;
     private XRGrabInteractable interactable;
+
+    public List<GameObject> productList;
+    public TMP_Text productNamesText; // Reference to the TMP_Text component to display the product names
+
     // Start is called before the first frame update
     [System.Obsolete]
     void Start()
@@ -24,6 +29,7 @@ public class Shoppingcart : MonoBehaviour
         // Activate the objectToActivate when the parent object is grabbed
         Debug.Log("TestSetActive");
         ui.SetActive(true);
+        UpdateProductNamesText();
     }
 
     private void OnSelectExited(XRBaseInteractor interactor)
@@ -31,6 +37,13 @@ public class Shoppingcart : MonoBehaviour
         // Deactivate the game object when the parent object is released
         Debug.Log("TestSetActiveFalse");
         ui.SetActive(false);
+
+        GameObject selectedObject = interactor.selectTarget.gameObject;
+        if (selectedObject.CompareTag("liquors_and_softdrinks") || selectedObject.CompareTag("housekeeping") || selectedObject.CompareTag("pastas_and_condiments"))
+        {
+            productList.Remove(selectedObject);
+            UpdateProductNamesText();
+        }
     }
 
     public void AddItemToCart()
@@ -50,6 +63,29 @@ public class Shoppingcart : MonoBehaviour
 
     private void UpdateItemCountText()
     {
-        itemCountText.text = "Item count: " + itemCount;
+        itemCountText.text = "Items: " + itemCount.ToString();
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("liquors_and_softdrinks") || collision.gameObject.CompareTag("housekeeping") || collision.gameObject.CompareTag("pastas_and_condiments"))
+        {
+            productList.Add(collision.gameObject);
+            Destroy(collision.gameObject);
+            UpdateProductNamesText();
+        }
+    }
+
+    private void UpdateProductNamesText()
+    {
+        string productNames = "Products: ";
+
+        foreach (GameObject product in productList)
+        {
+            string productName = product.name;
+            productNames += productName + ", ";
+        }
+
+        productNamesText.text = productNames.TrimEnd(',', ' '); // Trim the trailing comma and space
     }
 }
